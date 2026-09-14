c2f2074d65a7c58121f56db9fbcbe46061733b3a
SeongMyeongGyo <smksmk798@naver.com>
2026-09-14 22:27:44 +0900

    이벤트 기반 HP/MP UI 갱신을 위한 User Widget 생성

---
+++ Source/Framework/FWStatusBarWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "FWStatusBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRAMEWORK_API UFWStatusBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
