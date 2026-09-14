2a97a9ac17b59e749c48c08a1feabd9328e1f11b
SeongMyeongGyo <smksmk798@naver.com>
2026-09-14 22:35:37 +0900

    이벤트 기반 HP/MP UI 갱신 로직 추가
    BindWidget으로 HealthBar/ManaBar/HealthText/ManaText 연결
    AttributeComp 델리게이트 구독으로 값 변경 시 자동 갱신

--- Source/Framework/FWStatusBarWidget.h
+++ Source/Framework/FWStatusBarWidget.h
@@ -6,12 +6,48 @@
 #include "Blueprint/UserWidget.h"
 #include "FWStatusBarWidget.generated.h"
 
-/**
- * 
- */
+class UProgressBar;
+class UTextBlock;
+class UFWHealManaActorComponent;
+
 UCLASS()
 class FRAMEWORK_API UFWStatusBarWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+    virtual void NativeConstruct() override;
+    virtual void NativeDestruct() override;
+
+    /** 이 위젯이 표시할 대상 액터의 AttributeComponent에 바인딩 */
+    UFUNCTION(BlueprintCallable, Category = "FW|Status")
+    void BindToActor(AActor* Actor);
+
+protected:
+    // BP에서 이 이름의 위젯을 만들어야 함 (필수)
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UProgressBar> HealthBar;
+
+    UPROPERTY(meta = (BindWidget))
+    TObjectPtr<UProgressBar> ManaBar;
+
+    // 선택: 있으면 "75 / 100" 텍스트 표시
+    UPROPERTY(meta = (BindWidgetOptional))
+    TObjectPtr<UTextBlock> HealthText;
+
+    UPROPERTY(meta = (BindWidgetOptional))
+    TObjectPtr<UTextBlock> ManaText;
+
+    UPROPERTY()
+    TObjectPtr<UFWHealManaActorComponent> TrackedAttrComp;
+
+    UFUNCTION()
+    void HandleHealthChanged(UFWHealManaActorComponent* Comp, float NewValue, float MaxValue, float Delta);
+
+    UFUNCTION()
+    void HandleManaChanged(UFWHealManaActorComponent* Comp, float NewValue, float MaxValue, float Delta);
+
+    void RefreshHealthUI(float Current, float Max);
+    void RefreshManaUI(float Current, float Max);
 	
 };
